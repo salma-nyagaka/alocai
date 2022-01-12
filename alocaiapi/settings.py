c044f81c6723a61822d2998f4fe820d52d88c4fd
salmanyagaka <salmanyagaka@gmail.com>
2022-01-12 12:21:18 +0300

    add db settings

--- alocaiapi/settings.py
+++ alocaiapi/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'alocaiapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME", ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'USER': os.getenv('DB_USER', ''),
+        'PORT': os.getenv('DB_PORT', ''),
+        'HOST': os.getenv("DB_HOST", '')
+
     }
 }
 
